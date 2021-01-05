053cc4ad5ce30047f7ebc296869381044a6b5dcc
jonmanzanal <jonmanzanal@opendeusto.es>
2021-01-05 17:45:41 +0100

    Envio giroscopio

--- sender-v1.py
+++ sender-v1.py
@@ -1,8 +1,8 @@
 import time
 from grove.gpio import GPIO
 import paho.mqtt.client as mqtt
+from gyro import SensorITG3200
 
-  
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
@@ -10,19 +10,18 @@ def on_connect(client, userdata, flags, rc):
 gpio1 = 5
 gpio2 = 16
 gpio3 = 18
-gpio4 = 22
-gpio5 = 22
 
 sensor1 = GPIO(gpio1, GPIO.IN)
 sensor2 = GPIO(gpio2, GPIO.IN)
 sensor3 = GPIO(gpio3, GPIO.IN)
-sensor4 = GPIO(gpio4, GPIO.IN)
-sensor5 = GPIO(gpio5, GPIO.IN)
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.connect("192.168.1.105", 1883, 60)
 client.loop_start()
+
+sensor = SensorITG3200(1, 0x68)
+sensor.default_init()
 
 while True:
     status=""
@@ -38,18 +37,14 @@ while True:
         status+="H"
     else:
         status+="L"
-    if sensor4.read():
-        status+="H"
-    else:
+    gx, gy, gz = sensor.read_data()
+    if int(gy)>100:
+        status+="R"
+    elif int(gy)<-100:
         status+="L"
-    if sensor5.read():
-        status+="H"
     else:
-        status+="L"
-
+        status+="N"
+    
     print(status)
-    print('{}'.format(potencia.value))
-    time.sleep(0.1)
     client.publish("status",status)
     time.sleep(2)
-    
